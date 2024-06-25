5d5d97db628cf5370a5e05a4365ee801c2962fe0
230453 <230453@jc-21.jp>
2024-06-25 12:20:59 +0900

    課題１３やった

--- 課題６/main.cpp
+++ 課題６/main.cpp
@@ -1,14 +1,36 @@
 #include "Calclation.h"
+//プロトタイプ宣言
+void SetX(float a, float b);
+void SetY(float a, float b);
+//グローバル変数
 
+//メイン関数
 int main()
 {
-	float a = 42.195f, b = 3.14f;
-
-	//クラスのインスタンスを作成
-	Calclation x;
-
-	//メンバ関数を呼び出す
-	x.SetA(a);	//aの値を渡す
-	x.SetB(b);	//ｂの値を渡す
-	x.Disp();	//計算して結果を表示
+	//インスタンスＸの処理
+	SetX(5.0, 10.0);
+	
+	//インスタンスＹの処理
+	SetY(8.0, 3.0);
+	
+}
+//インスタンスＸのアクセス関数を呼ぶ
+void SetX(float a, float b)
+{
+	Calclation* x;
+	x = new Calclation();
+	x->SetA(a);
+	x->SetB(b);
+	x->Disp();
+	delete x;
+}
+//インスタンスＹのアクセス関数を呼ぶ
+void SetY(float a, float b)
+{
+	Calclation* y;
+	y = new Calclation();
+	y->SetA(a);
+	y->SetB(b);
+	y->Disp();
+	delete y;
 }
